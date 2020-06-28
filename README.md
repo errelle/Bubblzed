55d862d811d7bd40fb6e089f5862ebca5ef7d010
errelle <laetra.mos@laposte.net>
2020-06-28 16:32:57 +0200

    modif taille equipement ajout mp3 et commentaires

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Bubblzed
+jeu Bubblzed = cookie clicker
+Cliquer sur les bulles pour obtenir des points
+En fonction du nombre de points, déblocage d'options
--- bubble.js
+++ bubble.js
@@ -1,3 +1,18 @@
+//A REGLER : pb remplacment curseur par image debloquee + ajout option changment niveau/fond d'écran
+
+//musique
+function play(idPlayer, control) {
+    var player = document.querySelector('#' + idPlayer);
+    if (player.paused) {
+        player.play();
+        control.textContent = 'Pause';
+    } else {
+        player.pause();
+        control.textContent = 'Play';
+    }
+}
+
+// liens éléments html
 var jeu = document.getElementById('jeu');;
 var doigt = document.getElementById("doigt");
 var fouet = document.getElementById("fouet");
@@ -6,12 +21,13 @@ var loup = document.getElementById("loup");
 var score = document.getElementById("score");
 var gain = document.getElementById("gain");
 
-var images = [];
+/*essai curseur
 images[1] = {cursor : url("images/zombiefinger.png")};
-images[2] = "assets/shared/cursors/drum3.png";
+images[2] = "assets/shared/cursors/drum3.png";*/
 
-var clique = 0; 
-var compteur = 1; 
+//var calculs
+var clique = 0;
+var compteur = 1;
 var fric= 0;
 var prix = 20;
 var prix2 = 25;
@@ -19,11 +35,12 @@ var prix3 = 30;
 var prix4 = 40;
 
 
+//calcul points aléatoires par clic
 function ajoute() {
 	jeu;
 	clique = clique+compteur;
 	score.innerHTML = clique;
-	jeu = getRndInteger(0,10);	
+	jeu = getRndInteger(0,10);
 }
 
 function getRndInteger(min, max) {
@@ -31,6 +48,7 @@ function getRndInteger(min, max) {
    gain.innerHTML = fric;
 }
 
+//déblocage doigt zombie
 function incrementer() {
 			if (fric>=prix) {
 			fric=fric-prix;
@@ -41,8 +59,11 @@ function incrementer() {
 		else {
 			alert("Arrete de buller et clique! Il faut atteindre "+prix+" plumes pour debloquer ce bonus ;-)");
 		}
-		document.getElementById("b1") = images[1];
-	/*	document.getElementById("b2").style.cursor = "wait";
+
+
+  	/*	essai curseur
+    document.getElementById("b1") = images[1];
+	  document.getElementById("b2").style.cursor = "wait";
 		document.getElementById("b3").style.cursor = "wait";
 		document.getElementById("b4").style.cursor = "wait";
 		document.getElementById("b5").style.cursor = "wait";
@@ -57,7 +78,8 @@ function incrementer() {
 		document.getElementById("b14").style.cursor = "wait";*/
 }
 
-function indianaJones() { 
+//déblocage fouet
+function indianaJones() {
 	if (fric>=prix2) {
 			fric=fric-prix2;
 			prix2=prix2*2;
@@ -68,42 +90,37 @@ function indianaJones() {
 		else {
 			alert("Arrete de buller et clique! Il faut atteindre "+prix2+" plumes pour debloquer ce bonus ;-)");
 		}
-	
 }
-	
-function coup2Boule() { 
+
+//deblocage zizou
+function coup2Boule() {
 	if (fric>=prix3) {
 			fric=fric-prix3;
 			prix3=prix3*2;
 			gain.innerHTML = fric;
 			zidane.value = ("Coup de boule !");
 			console.log(fric);
-			
 		}
 		else {
 			alert("Arrete de buller et clique! Il faut atteindre "+prix3+" encore plus de gain pour debloquer ce bonus ;-)");
 		}
-	
 }
 
+// deblocage loup = boost
 function fluck() {
 			fric++;
 			document.getElementById("gain").innerHTML = fric;
 }
-
 function boost() {
-
 	if (fric>=prix4) {
-		setInterval("fluck()", 5000); 
+		setInterval("fluck()", 5000);
 		 fric=fric-prix4;
 		 prix4 = prix4 *2;
 		 console.log(fric);
 		document.getElementById("gain").innerHTML = fric;
 		}
-
 		else
-		{ 
+		{
 			alert("No pain, no gain!");
 		}
 }
-
